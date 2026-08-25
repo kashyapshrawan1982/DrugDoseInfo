887c8e3829be1da016a4c64637799cbe9f4d2ff2
kashyapshrawan1982 <kashyap.shrawan1982@gmail.com>
2026-08-25 11:14:07 +0530

    Update CalculationHistoryItem.kt

--- app/src/main/java/com/example/model/CalculationHistoryItem.kt
+++ app/src/main/java/com/example/model/CalculationHistoryItem.kt
@@ -1,5 +1,6 @@
 package com.example.model
 
+import com.example.model.CalculationHistoryItem
 data class CalculationHistoryItem(
     val id: String = java.util.UUID.randomUUID().toString(),
     val drugName: String,
